13b2cf094e4b976f60f4cbe3c261666d41328dd5
yuita-yoshihiko <soup998211@gmail.com>
2024-04-28 11:52:35 +0900

    新規登録画面の見た目作成

--- react-app/src/components/Signup.tsx
+++ react-app/src/components/Signup.tsx
@@ -1,80 +1,58 @@
 import { FC } from "react";
-import { HelmetProvider } from "react-helmet-async";
+import { Link } from 'react-router-dom';
 const Signup: FC = () => {
   return (
     <>
-      <HelmetProvider>
-        <link
-          rel="preconnect"
-          href="https://fonts.googleapis.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=DotGothic16&family=Kanit:wght@900&family=Sawarabi+Mincho&display=swap"
-          rel="stylesheet"
-        />
-      </HelmetProvider>
-      <div className="relative overflow-hidden bg-[#caaca6] h-screen ">
-        <div
-          className="font-kanit text-10xl font-black bg-cover bg-clip-text text-transparent pt-0 pl-3 bg-[url('../public/brick-wall.jpg')]"
-          style={{ color: "#944d74a9" }}
-        >
-          {/* tailwindcssにはtextとbgしか色設定がない。それ以外の色設定はstyleでカラーコードを直接入力するしかない */}
-          SIGNUP
-          <div className="absolute bg-white w-[65vw] h-[130vh] -top-32 right-0 rounded-l-full flex justify-center items-center ">
-            <form className="font-dotgothic min-w-[65%] items-end">
-              <div className="flex flex-col text-gray-500">
-                <div className="flex justify-end items-center">
-                  <h1 className=" text-5xl mb-20">SIGN UP</h1>
-                </div>
-                <input
-                  className="font-thin text-xl tracking-widest mb-3.5 leading-10 border-b-2 border-b-transparent hover:border-b-[#caaca6] focus:outline-none"
-                  id="Name"
-                  type="text"
-                  placeholder="Name"
-                />
-                <input
-                  className="font-thin text-xl tracking-widest mb-3.5 leading-10 border-b-2 border-b-transparent hover:border-b-[#caaca6] focus:outline-none"
-                  id="email"
-                  type="text"
-                  placeholder="Email"
-                />
-                <input
-                  className="font-thin text-xl tracking-widest mb-3.5 leading-10 border-b-2 border-b-transparent hover:border-b-[#caaca6] focus:outline-none"
-                  id="password1"
-                  type="password"
-                  placeholder="Password"
-                />
-                <input
-                  className="font-thin text-xl tracking-widest mb-3.5 leading-10 border-b-2 border-b-transparent hover:border-b-[#caaca6] focus:outline-none"
-                  id="password2"
-                  type="password"
-                  placeholder="Password Confirmation"
-                />
-                <div className="flex justify-end items-center">
-                  <button className="bg-[#caaca6] hover:bg-[#c18a80] text-white rounded-full px-16 py-4 text-xl mb-3.5">
-                    SIGN UP
-                  </button>
-                </div>
-                <div className="flex justify-end items-center">
-                  <a
-                    href="/"
-                    aria-label="Sign up"
-                    title="Sign up"
-                    className="text-xl"
-                  >
-                    Already have an account?
-                  </a>
-                </div>
+      <div className="flex justify-center items-center bg-cyan-200 min-h-screen">
+        <form className="items-end w-1/3">
+          <div className="flex flex-col text-gray-500">
+            <div className="text-center text-2xl font-bold mb-10">
+              新規登録
+            </div>
+              <div className="font-bold text-left mb-2">
+                ユーザー名
               </div>
-            </form>
+              <input
+                className="bg-cyan-50 text-xl mb-10 p-2.5 focus:outline-none rounded-lg"
+                id="Name"
+                type="text"
+              />
+              <div className="text-left font-bold mb-2">
+                メールアドレス
+              </div>
+              <input
+                className="bg-cyan-50 text-xl mb-10 p-2.5 focus:outline-none rounded-lg"
+                id="email"
+                type="text"
+              />
+              <div className="text-left font-bold mb-2">
+                パスワード
+              </div>
+              <input
+                className="bg-cyan-50 text-xl mb-10 p-2.5 focus:outline-none rounded-lg"
+                id="password1"
+                type="password"
+              />
+              <div className="text-left font-bold mb-2">
+                パスワード（確認用）
+              </div>
+              <input
+                className="bg-cyan-50 text-xl mb-10 p-2.5 focus:outline-none rounded-lg"
+                id="password2"
+                type="password"
+              />
+            <div className="text-center font-bold mb-10">
+            <Link to="/login" className="hover:underline">
+              ログインはこちら
+            </Link>
+            </div>
+            <div className="flex justify-center">
+              <button className="w-1/4 focus:outline-none bg-yellow-400 hover:bg-yellow-500 font-bold p-3 rounded-lg">
+                新規登録
+              </button>
+            </div>
           </div>
-        </div>
+        </form>
       </div>
     </>
   );
