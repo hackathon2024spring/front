eafe88257ceb0646537a2c236800e8a5c62b8e4f
mizuno22 <gabeko22@gmail.com>
2024-05-18 07:19:56 +0900

    エクササイズ設定ページとユーザー詳細ページをレスポンシブ対応にした

--- react-app/src/components/User.tsx
+++ react-app/src/components/User.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState, useEffect } from 'react';
+import { FC, useState, useEffect } from 'react';
 import { BaseURL } from '../utilities/base_url';
 import { Link, useNavigate } from 'react-router-dom';
 
@@ -6,6 +6,7 @@ interface User {
   username: string;
   email: string;
 }
+
 const User: FC = () => {
   const [user, setUser] = useState<User>();
   const [loading, setLoading] = useState(true);
@@ -50,13 +51,13 @@ const User: FC = () => {
   return (
     <>
       <div className="flex justify-center items-center bg-cyan-200 min-h-screen">
-        <form className="w-1/3">
-          <div className="container mx-auto flex flex-col text-gray-500 bg-cyan-50 rounded-lg m-10">
+        <form className="w-full max-w-xl p-4 h-3/4"> 
+          <div className="container mx-auto flex flex-col text-gray-500 bg-cyan-50 rounded-lg p-16 h-full"> 
             <div className="text-center text-2xl font-bold mb-10 mt-10">
               ユーザー情報
             </div>
             <div className="flex justify-center">
-              <div className="flex flex-col w-1/2 mb-10">
+              <div className="flex flex-col w-full mb-10">
                 <div className="text-left font-bold mb-2">
                   ユーザー名
                 </div>
@@ -66,7 +67,7 @@ const User: FC = () => {
               </div>
             </div>
             <div className="flex justify-center">
-              <div className="flex flex-col w-1/2 mb-10">
+              <div className="flex flex-col w-full mb-10">
                 <div className="text-left font-bold mb-2">
                   メールアドレス
                 </div>
