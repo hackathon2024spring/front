37b8cbc5cf8c4d432282a7025906814928224f99
yuita-yoshihiko <soup998211@gmail.com>
2024-05-17 22:27:36 +0900

    サインアウト後の遷移先変更とフラッシュメッセージの実装

--- react-app/src/components/CalendarHeader.tsx
+++ react-app/src/components/CalendarHeader.tsx
@@ -37,7 +37,7 @@ const CalendarHeader: FC = () => {
         },
       });
       if (response.ok) {
-        navigate('/signup');
+        navigate('/login', { state: { message: 'サインアウトしました' } });
       } else {
         console.error('サインアウトに失敗しました');
       }
